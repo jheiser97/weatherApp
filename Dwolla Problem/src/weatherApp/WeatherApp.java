628429168def1c7a9f77edee64c93b2771017bba
Josh Heiser <jheiser@iastate.edu>
2019-02-12 18:31:20 -0600

    basic weather app functionality working
    Weather app correctly working minus testing through gitHub

---
+++ Dwolla Problem/src/weatherApp/WeatherApp.java
@@ -0,0 +1,209 @@
+package weatherApp;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.util.Scanner;
+
+import org.apache.commons.io.IOUtils;
+import org.json.*;
+public class WeatherApp {
+	static boolean convert = false;	
+	
+	public static void main(String[] args) {
+		while (true){
+			//string that keeps track of user input. This input will either be a city or a signal to end the program
+			String location = "";
+			System.out.println("Please Enter a city to get its current weather conditions or type exit to end program");
+			Scanner s = new Scanner (System.in);
+			//waits for the user to place input
+			while (location.equals("")){
+				location+= s.nextLine();
+				//checks if it is empty or null (this prevents the API from searching for a blank name or NULL for the city)
+				if (location.equals("") || location.equals(null)){
+					System.out.println("This is an invalid input, please insert another city.");
+				}
+				//this checks if the user entered exit. If they did, then the program should end
+				if (location.equals("exit") || location.equals("Exit")|| location.equals("EXIT")){
+					System.exit(0);
+		
+				}
+				System.out.println("Do you want SI (Celsius, m/s) or Imperial(Farenheit, mph)?");
+				System.out.println("Yes = SI, No = Imperial. This program defaults to Imperial if anything besides Yes is entered.");
+				//variable that keeps track of the unit that the user wants the weather to return as
+				String unit = s.nextLine();
+				//if the user has inputted yes, then we want the values to return as SI and thus needs to be converted
+				if (unit.equals("yes") || unit.equals("YES") || unit.equals("Yes")){
+					convert = true;
+				}
+			}
+			getWeatherConditions(location);
+			//This resets the conversion to false. This is so if the user wants to lookup another city, it will not automatially 
+			//give the user SI units (since it would be true always otherwise).
+			convert = false;
+		}
+	}
+	
+	public static void getWeatherConditions (String location) {
+		try {
+			//Beginning part of the url that is used to connect to the OpenWeather API to get our data from
+			String url = "http://api.openweathermap.org/data/2.5/weather?q=";
+			//API key used to connect to the OpenWeather API
+			String API = "&APPID=844107e94c850ace5d28ba5043d4e625";
+			//the total URL that will sent to the OpenWeather API
+			String updatedURL = url + location + API;
+			//goes to the URL, and stores the returned JSON as a JSONObject
+			JSONObject json = new JSONObject(IOUtils.toString(new URL(updatedURL), Charset.forName("UTF-8")));
+			
+			//gets the current condition at the location that the user has speicifed
+			JSONObject condition = json.getJSONArray("weather").getJSONObject(0);
+			System.out.println("The current condition in " + location + " is " + condition.getString("description"));
+			
+			//this gets the current temperature
+			JSONObject temperature = json.getJSONObject("main");
+			int temp = temperature.getInt("temp");
+			temp = ConvertTemperature(temp);
+			
+			//gets the current windspeed
+			JSONObject windSpeed = json.getJSONObject("wind");
+			int speed = windSpeed.getInt("speed");
+			int directionDegree = windSpeed.getInt("deg");
+			speed = ConvertWindSpeed(speed);
+			String direction = FindWindDirection(directionDegree);
+			
+			//the maximum temperature is found below
+			JSONObject tempMax = json.getJSONObject("main");
+			int maxTemp = tempMax.getInt("temp_max");
+			maxTemp = ConvertTemperature(maxTemp);
+			
+			//the minimum temperature is found below
+			JSONObject tempMin = json.getJSONObject("main");
+			int minTemp = tempMin.getInt("temp_min");
+			minTemp = ConvertTemperature(minTemp);
+			
+			//checks if the user has specified for the data to be returned as SI. If so, it will print the statements in this loop
+			if (convert){
+				System.out.println("The current temperature is " + temp +" degrees celsius");
+				System.out.println("The current windspeed is from the " + direction + " at " + speed + " m/s");
+				System.out.println("The High temperature was " + maxTemp + " C");
+				System.out.println("The low temperature was " + minTemp + " C");
+			}
+			//since the user has not specified that it should be in SI, it will be returned in Imperial units instead.
+			else {
+				System.out.println("The current temperature is " + temp + " F");
+				System.out.println("The current windspeed is from the " + direction + " at " + speed + " mph");
+				System.out.println("The High temperature was " + maxTemp + " F");
+				System.out.println("The low temperature was " + minTemp + " F");
+
+
+			}
+		}
+		catch (MalformedURLException m){
+			System.out.println("An error occured with the URL passed through the API. Additional information is shown below");
+			m.printStackTrace();
+		}
+		catch (JSONException j){
+			System.out.println("The JSON did not return as expected. The error location is displayed below.");
+			j.printStackTrace();
+		}
+		catch (IOException e){
+			System.out.println("Oops! That city doesn't exist. Try again.");
+		}
+	}
+	
+	/**
+	 * This method converts the temperature from Kelvin to Celsius or Farenheit (depends on user preference)
+	 * @param temperature- The temperature that is returned from the OpenWeather API
+	 * @return
+	 */
+	public static int ConvertTemperature (int temperature){
+		//checks if the user wanted the temperature to be in SI.
+		if (convert) {
+			//if so, converts to SI units
+			temperature = temperature - 273;
+			return temperature;
+		}
+		//since the user does not want the value to be in SI, it is converted to imperial units
+		temperature = temperature - 273;
+		temperature = ((temperature * (9/5)) + 32);
+		return temperature;
+	}
+	
+	/**
+	 * This method takes in the windSpeed that was returned from the OpenWeather API and converts it to the type of weather
+	 * that the user has specified
+	 * @param windSpeed - The windspeed that was returned by the OpenWeather API
+	 * @return
+	 */
+	public static int ConvertWindSpeed (int windSpeed){
+		//checks to see if the user wants the values to be returned SI. if so, then the value returned by the OpenWeather API does not be changed.
+		if (convert){
+			return windSpeed;
+		}
+		//since it is not in SI, it needs to be converted to Imperial units.
+		return windSpeed = (int) (windSpeed * 2.237);
+	}
+	
+	/**
+	 * This method converts the int that represents the degree that the wind is coming from and converts it to a String that represents the direction
+	 * that the wind is coming from
+	 * @param directionDegree - Integer that represents the degree that the wind is coming from (from 0-360)
+	 * @return
+	 */
+	public static String FindWindDirection (int directionDegree){
+		//these values were calculated using this URL: http://snowfence.umn.edu/Components/winddirectionanddegreeswithouttable3.htm
+		//depending on the angle that is given to use by the OpenWeather API, it will represent a certain direction
+		//we can use this to determine and return the direction that the wind is blowing from.
+		if (directionDegree < 11 || directionDegree > 348){
+			return "North";
+		}
+		if (directionDegree >= 11 && directionDegree <= 33){
+			return "NNE";
+		}
+		if (directionDegree > 33 && directionDegree < 56){
+			return "NE";
+		}
+		if (directionDegree >= 56 && directionDegree < 79){
+			return "ENE";
+		}
+		if (directionDegree >= 79 && directionDegree < 101){
+			return "East";
+		}
+		if (directionDegree >= 101 && directionDegree < 124){
+			return "ESE";
+		}
+		if (directionDegree >= 124 && directionDegree < 146){
+			return "SE";
+		}
+		if (directionDegree >= 146 && directionDegree < 169){
+			return "SSE";
+		}
+		if (directionDegree >= 169 && directionDegree <= 191){
+			return "South";
+		}
+		if (directionDegree > 191 && directionDegree <= 214){
+			return "SSW";
+		}
+		if (directionDegree > 214 && directionDegree < 236){
+			return "SW";
+		}
+		if (directionDegree >= 236 && directionDegree <= 259){
+			return "WSW";
+		}
+		if (directionDegree > 259 && directionDegree < 281){
+			return "West";
+		}
+		if (directionDegree >= 281 && directionDegree <= 304 ){
+			return "WNW";
+		}
+		if (directionDegree > 304 && directionDegree < 326){
+			return "NW";
+		}
+		if (directionDegree >= 326 && directionDegree <= 348 ){
+			return "WNW";
+		}
+		return "This is not a proper angle that can be used to calculate wind direction";
+	}
+}
